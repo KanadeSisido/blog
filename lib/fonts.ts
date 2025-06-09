4b1d33a8300266404d708f6173929b7a96fd9bcd
Kanadesisido <takasuyari@gmail.com>
2025-06-10 00:50:53 +0900

    fix: add- latin

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -2,16 +2,20 @@ import { Noto_Sans_JP, Roboto_Mono } from "next/font/google";
 
 export const noto_sans_jp_300 = Noto_Sans_JP({
 	weight: "300",
+	subsets: ["latin"],
 });
 
 export const noto_sans_jp_400 = Noto_Sans_JP({
 	weight: "400",
+	subsets: ["latin"],
 });
 
 export const noto_sans_jp_500 = Noto_Sans_JP({
 	weight: "500",
+	subsets: ["latin"],
 });
 
 export const roboto_mono_400 = Roboto_Mono({
 	weight: "400",
+	subsets: ["latin"],
 });
